dc3a161360f7940b7963692a77de0f0bd9ce1f76
DavisNova <useself206522561@gmail.com>
2025-10-01 10:51:39 +0800

    2

--- build/pdf_render/intermediates/cxx/Debug/5r2h1ia3/logs/arm64-v8a/build_command_bbhelper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Administrator\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Administrator\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\pdf_render-1.4.12\\android\\.cxx\\Debug\\5r2h1ia3\\arm64-v8a" ^
-  bbhelper
--- build/pdf_render/intermediates/cxx/RelWithDebInfo/3hv1j5m3/logs/x86_64/build_command_bbhelper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Administrator\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Administrator\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\pdf_render-1.4.12\\android\\.cxx\\RelWithDebInfo\\3hv1j5m3\\x86_64" ^
-  bbhelper
